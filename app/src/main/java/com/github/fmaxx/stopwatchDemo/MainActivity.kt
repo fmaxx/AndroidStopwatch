746d3d65e250f7b2998183d3c92e59bbb15bdac7
Maxim Firsov <firsoffmaxim@gmail.com>
2022-11-15 16:14:09 +0500

    Try to publish

--- app/src/main/java/com/github/fmaxx/stopwatchDemo/MainActivity.kt
+++ app/src/main/java/com/github/fmaxx/stopwatchDemo/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.github.fmaxx.stopwatch
+package com.github.fmaxx.stopwatchDemo
 
 import android.os.Bundle
 import android.widget.Button
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.github.fmaxx.stopwatch.Stopwatch
+import com.github.fmaxx.stopwatch.StopwatchProcessor
 import com.github.fmaxx.stopwatch.time.TimestampMillisecondsFormatter
 import kotlinx.coroutines.launch
 
